98f6b1c3677e0df483f7aa3a5f9f4d6e45925053
yehongyu <yehongyu1234@126.com>
2018-12-29 21:09:26 +0800

    project nice update

--- Modules/Project/Resources/views/nice.blade.php
+++ Modules/Project/Resources/views/nice.blade.php
@@ -1,11 +1,61 @@
 @extends('project::layouts.master')
 @section('content')
-    <h1>中民筑友项目信息显示</h1>
-    <p>
+<img src="http://pic9.nipic.com/20100915/3320946_082429132223_2.jpg" width="500px">
+<p>
+<div class="row">
+<div class="col-xs-12 col-sm-6 col-md-8">
+    <h2>项目简介</h2>
+    <hr width="100%" color="#bbe6ff">
+    {{$field->body}}
+</div>
+<div class="col-md-12">
+    <h2>工程概况</h2>
+    <hr width="100%" color="#bbe6ff">
+    <label>项目名称：</label>{{$field->name}}
+    <br>
+    <label>建设地点：</label>{{$field->location}}
+    <br>
+    <label>建设单位：</label>{{$field->name}}
+    <br>
+    <label>设计单位：</label>{{$field->name}}
+    <br>
+    <label>施工单位：</label>{{$field->name}}
+    <br>
+    <label>监理单位：</label>{{$field->name}}
+    <br>
+    <label>建筑面积：</label>{{$field->area}}㎡
+    <br>
+    <label>结构类别：</label>{{$field->structure_type}}
+</div>
+<div>
+    <h2>项目监护</h2>
+    <hr width="100%" color="#bbe6ff">
+    <label>项目经理：</label>{{$field->name}}
+    <br>
+    <label>生产经理：</label>{{$field->location}}
+    <br>
+    <label>技术经理：</label>{{$field->name}}
+    <br>
+    <label>施工员：</label>{{$field->name}}
+    <br>
+    <label>安全员：</label>{{$field->name}}
+    <br>
+    <label>物资材料员：</label>{{$field->name}}
+    <br>
+    <label>测量员：</label>{{$field->area}}㎡
+    <br>
+    <label>项目总监：</label>{{$field->structure_type}}
+</div>
+    <div>
+        <h2>项目信息</h2>
+        <hr width="100%" color="#bbe6ff">
+        <a href="#">安全检查项</a>
         <br>
-        项目id为{{$field->id}}
+        <a href="#">质量检查项</a>
         <br>
-        项目名称为{{$field->name}}
-        <br>
-    </p>
+        <a href="#">项目通讯录</a><br>
+        <a href="#">查看详情</a><br>
+    </div>
+</div>
+    <div><img href=""></div>
 @stop
